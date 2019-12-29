ea4f4afabd284df6c3301bc761427a4033234453
Luisa Ferreira <luisaaferreira23@gmail.com>
2019-12-29 01:33:29 -0300

    conexão com o banco

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import firebaseConfig from './firebase';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AngularFirestore
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/firebase.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBK8X804jopKKl3ox7j4J7XmQIlMHqDFKA",
+    authDomain: "shopapp-29.firebaseapp.com",
+    databaseURL: "https://shopapp-29.firebaseio.com",
+    projectId: "shopapp-29",
+    storageBucket: "shopapp-29.appspot.com",
+    messagingSenderId: "587284114611",
+    appId: "1:587284114611:web:621e1acc0caf072d9f94c6",
+    measurementId: "G-FDH1LR4D2V"
+  };
+
+  export default firebaseConfig;
